bb3fc9a31cf26819bfb40918ae523b3748510a67
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-12-11 10:35:29 -0700

    Normalize Tabbing

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ app.set('view engine', 'ejd')
 
 app.use(Express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+	console.log(`Listening at http://localhost:${port}`);
 })
