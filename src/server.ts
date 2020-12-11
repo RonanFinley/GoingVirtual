253ebb80c2ae549b4ed472805ef24d16914995d8
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-12-11 10:25:02 -0700

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import Express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+
+const app = Express()
+const port = 3000;
+
+app.set('view engine', 'ejd')
+
+app.use(Express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+})
